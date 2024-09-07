f615de7792afe704c45679a33b517dd5a00e1dc8
nikhilpakhloo <nikhilpakhloo@gmail.com>
2024-09-07 20:48:48 +0530

    webhook copied

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,16 +1,18 @@
-"use server"
-import User from "./model/user.model";
+"use server";
+import User, { IUser } from "./model/user.model";
 import { connectToDB } from "../db";
-export async function createUser(user:any) {
-    try {
-        await connectToDB();
-        const newUser = await User.create(user) ;
-        console.log(newUser)
-        return JSON.parse(JSON.stringify(newUser))
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
-    
+
+export async function createUser(user: Partial<IUser>) {
+  try {
+    await connectToDB();
+
+    const newUser = await User.create(user);
+
+    console.log(newUser);
+
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw new Error("User creation failed");
+  }
 }
