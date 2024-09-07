5f069a6df4a9363fa6d9b2b614aaeb3bcea578ed
nikhilpakhloo <nikhilpakhloo@gmail.com>
2024-09-07 20:37:47 +0530

    webhook copied

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,6 +5,7 @@ export async function createUser(user:any) {
     try {
         await connectToDB();
         const newUser = await User.create(user) ;
+        console.log(newUser)
         return JSON.parse(JSON.stringify(newUser))
         
     } catch (error) {
